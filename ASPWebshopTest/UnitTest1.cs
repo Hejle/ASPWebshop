88d9d44960e0b79396f5c5aa7ffdf35cfce00dc3
Joachim Hejlesen <joah@netcompany.com>
2021-06-17 11:39:33 +0200

    Added Unittest framework

---
+++ ASPWebshopTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ASPWebshopTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
