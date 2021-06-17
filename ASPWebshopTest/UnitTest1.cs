728e335882f9aa60908d6348c0bf4194bf6d9150
Hejle <joachim-dk@hotmail.com>
2021-06-17 19:57:18 +0200

    Update UnitTest1.cs
    Updated test

--- ASPWebshopTest/UnitTest1.cs
+++ ASPWebshopTest/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace ASPWebshopTest
         public void TestMethod2()
         {
             var testString = string.Empty;
-            Assert.IsNotNull(testString);
+            Assert.IsNull(testString);
         }
     }
 }
