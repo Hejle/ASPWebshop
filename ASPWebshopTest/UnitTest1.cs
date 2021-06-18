e3227f554e42632ee498f8e22e8e615bb8e4f323
Hejle <joachim-dk@hotmail.com>
2021-06-18 21:22:26 +0200

    Added Unittest framework (#3)
    * Added Unittest framework

---
+++ ASPWebshopTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ASPWebshopTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string testString = null;
+            Assert.IsNull(testString);
+        }
+        
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var testString = string.Empty;
+            Assert.IsNotNull(testString);
+        }
+    }
+}
