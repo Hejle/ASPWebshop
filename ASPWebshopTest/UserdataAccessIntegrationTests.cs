8fc23a17cd85a0db41679781c551242243544f03
Hejle <joachim-dk@hotmail.com>
2021-07-17 08:34:44 +0200

    Added custom IntegrationTest attribute

--- ASPWebshopTest/UserdataAccessIntegrationTests.cs
+++ ASPWebshopTest/UserdataAccessIntegrationTests.cs
@@ -2,6 +2,7 @@
 using ASPWebshop.Pages.Models;
 using ASPWebshop.Services.Implementations;
 using ASPWebshop.Services.Interfaces;
+using ASPWebshopTest.TestAttributes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
@@ -13,6 +14,17 @@ namespace ASPWebshopTest
     {
         [TestMethod, TestCategory("IntegrationTest")]
         public void ConnectedToDatabase_WithCorrectCredentials_ShouldReturnAConnection()
+        {
+            throw new NotImplementedException();
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod, IntegrationTest]
+        public void ConnectedToDatabase_WithIncorrectCredentials_ShouldThrowSomeError()
         {
             throw new NotImplementedException();
             //Arrange
