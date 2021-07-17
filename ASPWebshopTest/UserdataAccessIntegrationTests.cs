36a07c886a26d09313e27ee54a47fb3fabb7e9d4
Hejle <joachim-dk@hotmail.com>
2021-07-17 07:36:41 +0200

    Feature/#7 build script ignores integrationtest (#8)
    #7 Added first IntegrationTest and updated buildscript. Closes #7

---
+++ ASPWebshopTest/UserdataAccessIntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿using ASPWebshop.Exceptions;
+using ASPWebshop.Pages.Models;
+using ASPWebshop.Services.Implementations;
+using ASPWebshop.Services.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace ASPWebshopTest
+{
+    [TestClass]
+    public class UserdataAccessIntegrationTests
+    {
+        [TestMethod, TestCategory("IntegrationTest")]
+        public void ConnectedToDatabase_WithCorrectCredentials_ShouldReturnAConnection()
+        {
+            throw new NotImplementedException();
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+    }
+}
